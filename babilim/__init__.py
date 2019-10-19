28db93e73af31684553e6ba969e59318b5ed2d87
Michael Fuerst <mail@michaelfuerst.de>
2019-10-19 14:32:22 +0200

    Improved documentation by adding introductions to all packages.

--- babilim/__init__.py
+++ babilim/__init__.py
@@ -20,8 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__all__ = ['PHASE_TRAIN', 'PHASE_VALIDATION', 'PHASE_TRAINVAL',
-           'PHASE_TEST', 'PYTORCH_BACKEND', 'TF_BACKEND', 'set_backend', 'get_backend', 'is_backend', 'tprint']
+__all__ = ['tprint', 'set_backend', 'get_backend', 'is_backend']
 
 
 import time as __time
@@ -39,7 +38,21 @@ TF_BACKEND = "tf2"
 _backend = PYTORCH_BACKEND
 
 
-def tprint(msg: str, end: str="\n"):
+def tprint(msg: str, end: str="\n") -> None:
+    """
+    Print something with a timestamp.
+    Useful for logging.
+    Babilim internally uses this for all its log messages.
+
+    .. code-block:: python
+
+        from babilim import tprint
+        tprint("This is a log message.")
+        # [yyyy-mm-dd HH:MM:SS] This is a log message.
+
+    :param msg: The message to print.
+    :param end: The line ending. Defaults to "\n" but can be set to "" to not have a linebreak.
+    """
     time_stamp = __datetime.datetime.fromtimestamp(__time.time()).strftime('%Y-%m-%d %H:%M:%S')
     print("\r[{}] {}".format(time_stamp, msg), end=end)
 
@@ -49,6 +62,13 @@ def set_backend(backend: str):
     Set the backend which babilim uses.
 
     Should be either babilim.PYTORCH_BACKEND or babilim.TF_BACKEND.
+
+    .. code-block:: python
+
+        import babilim
+        babilim.set_backend(babilim.PYTORCH_BACKEND)
+        # or
+        babilim.set_backend(babilim.TF_BACKEND)
     
     :param backend: The backend which should be used.
     :type backend: str
@@ -71,20 +91,35 @@ def set_backend(backend: str):
 
 
 def get_backend() -> str:
-    """foobar
+    """
+    Get what backend is currently set.
+
+    .. code-block:: python
+
+        import babilim
+        print(babilim.get_backend())
     
-    :return: [description]
+    :return: The backend string.
     :rtype: str
     """
     return _backend
 
 
 def is_backend(backend: str) -> bool:
-    """Foo
+    """
+    Check if the currently set backend is the one to check against.
+
+
+    .. code-block:: python
+
+        import babilim
+        if babilim.is_backend(babilim.PYTORCH_BACKEND):
+            # do pytorch specific stuff
+            pass
     
-    :param backend: [description]
+    :param backend: The backend to check against
     :type backend: str
-    :return: [description]
+    :return: True if the set backend is equal to the provided backend.
     :rtype: bool
     """
     return _backend == backend
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,15 +1,66 @@
-Babilim - Develop for tf2 or pytorch
-====================================
+Babilim - Deep Learning across Pytorch and TF2
+==============================================
 
-TODO general hello there.
+TODO Teaser Image with Logo of babilim.
+
+What is Babilim?
+----------------
+
+Babilim is a Deep Learning Framework designed for ease of use like Keras.
+The API is designed to be intuitive and easy while allowing for complex production or research models.
+On top of that babilim runs on top of Tensorflow 2 or Pytorch, whichever you prefer.
+
+Babilim was designed for:
+
+* Intuitive ease of use,
+* a unified development experience across pytorch and tf2,
+* flexibility for research and robustness for production
+
+
+Selecting your backend
+----------------------
+
+Since babilim is designed for multiple backends (tensorflow 2 and pytorch), it gives you the choice which should be used.
+When your company/university uses one of the two frameworks you are fine to use or you can follow your personal preference for private projects.
+
+.. code-block:: python
+
+    import babilim
+    babilim.set_backend(babilim.PYTORCH_BACKEND)
+    # or
+    babilim.set_backend(babilim.TF_BACKEND)
+
+
+Design Principles
+-----------------
+
+TODO
+
+Tutorials & Examples
+--------------------
+
+TODO
+
+Why called babilim?
+-------------------
+
+**TL;DR** Referencing to Tower of Babel.
+
+The Tower of Babel narrative in Genesis 11:1-9 is a myth about mankind building a tower that could reach into the heavens.
+However, the attempt gets set back and fails because they started speaking different languages and were no longer able to understand each other.
+
+Luckily for AI development there is only two major frameworks which share nearly all market share.
+Babilim is an attempt to unite the two to talk in a language compatible with both.
 
 .. toctree::
    :caption: Getting Started
+   :includehidden:
 
    tutorials.install
 
 .. toctree::
    :caption: Packages
+   :hidden:
 
    babilim
    babilim.callbacks
@@ -25,6 +76,7 @@ TODO general hello there.
 
 .. toctree::
    :caption: Examples
+   :hidden:
 
    examples.mnist
 

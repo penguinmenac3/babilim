7f8e8ce284bed17551824f5bee40db573d7b1feb
Michael Fuerst <mail@michaelfuerst.de>
2020-01-16 23:34:28 +0100

    Improved documentation.

--- babilim/_nbdev.py
+++ babilim/_nbdev.py
@@ -3,9 +3,16 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"ClassDecorator": "core_annotations.ipynb",
-         "RunOnlyOnce": "core_annotations.ipynb"}
+         "RunOnlyOnce": "core_annotations.ipynb",
+         "ConfigPart": "core_config.ipynb",
+         "Config": "core_config.ipynb",
+         "import_config": "core_config.ipynb",
+         "import_checkpoint_config": "core_config.ipynb",
+         "Module": "model_module.ipynb"}
 
-modules = ["corennotations.py"]
+modules = ["corennotations.py",
+           "core\config.py",
+           "model\module.py"]
 
 git_url = "https://github.com/penguinmenac3/babilim/tree/master/"
 

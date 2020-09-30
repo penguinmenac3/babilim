539ae73784a25f6cba85e78281ea3fa5385f588c
Michael Fuerst <mail@michaelfuerst.de>
2020-09-30 22:01:16 +0200

    Added dimension selection to gather.

--- babilim/model/layers/selection.py
+++ babilim/model/layers/selection.py
@@ -14,7 +14,7 @@ from babilim.core.module_native import ModuleNative
 
 # Cell: 2
 class Gather(ModuleNative):
-    def __init__(self):
+    def __init__(self, axis):
         """
         Gather tensors from one tensor by providing an index tensor.
     
@@ -22,8 +22,15 @@ class Gather(ModuleNative):
         * **input_tensor**: (Tensor[N, L, ?]) The tensor from which to gather values at the given indices.
         * **indices**: (Tensor[N, K]) The indices at which to return the values of the input tensor.
         * **returns**: (Tensor[N, K, ?]) The tensor containing the values at the indices given.
+
+        Arguments:
+        :param axis: The axis along which to select.
         """
         super().__init__()
+        assert axis != 0, "You cannot gather over the batch dimension."
+        if axis > 0:
+            axis = axis - 1
+        self.axis = axis
         
     @RunOnlyOnce
     def _build_pytorch(self, input_tensor, indices):
@@ -34,7 +41,7 @@ class Gather(ModuleNative):
         assert input_tensor.shape[0] == indices.shape[0]
 
         # Then gather the indices along the batches.
-        return torch.stack([torch.index_select(input_tensor[i], 0, indices[i]) for i in range(indices.shape[0])])
+        return torch.stack([torch.index_select(input_tensor[i], self.axis, indices[i]) for i in range(indices.shape[0])])
     
     @RunOnlyOnce
     def _build_tf(self, input_tensor, indices):
